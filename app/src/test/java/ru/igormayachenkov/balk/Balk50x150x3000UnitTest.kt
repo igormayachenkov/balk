c4299aa0e28c9578c3f13681df472214e1e385b2
igor <igor.mayachenkov@gmail.com>
2026-02-20 17:55:34 +0300

    domain unit test added

---
+++ app/src/test/java/ru/igormayachenkov/balk/Balk50x150x3000UnitTest.kt
@@ -0,0 +1,37 @@
+package ru.igormayachenkov.balk
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import ru.igormayachenkov.balk.data.Balk
+import ru.igormayachenkov.balk.domain.CalculateUseCase
+import ru.igormayachenkov.balk.domain.Calculation
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class Balk50x150x3000UnitTest {
+    lateinit var calculation: Calculation.Success
+
+    @Before
+    fun calculate(){
+        val balk = Balk(
+            width  = 0.05,
+            height = 0.15,
+            length = 3.0
+        )
+        println("*** balk: $balk")
+
+        calculation = CalculateUseCase(balk)() as Calculation.Success
+    }
+
+    @Test
+    fun deformation_isCorrect() {
+        println("*** deformation: ${calculation.deformation}")
+        assertEquals(0.005, calculation.deformation, 0.001 )
+    }
+
+}
--- app/src/test/java/ru/igormayachenkov/balk/ExampleUnitTest.kt
+++ app/src/test/java/ru/igormayachenkov/balk/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package ru.igormayachenkov.balk
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,25 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    init {
+        println("--- init ---")
+    }
+
+    @Before
+    fun initTest(){
+        println("=== Before ===")
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        println("*** case addition ***")
     }
+
+    @Test
+    fun case2_isCorrect() {
+        assertEquals(4, 2 + 2)
+        println("*** case 2 ***")
+    }
+
 }
